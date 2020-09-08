19eb84136c243223e73a8c966ba4cdf717c6e4f4
Maggoff <magmbov@gmail.com>
2020-09-08 14:14:10 +0300

    Da

---
+++ js/main.js
@@ -0,0 +1,84 @@
+let id = document.getElementsByClassName("test__menu__item");
+let img = document.getElementsByClassName("test__item");
+for (let i=0; i<id.length; i++){
+  if(i==0){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==1){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==2){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==3){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==4){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==5){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==6){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==7){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==8){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+  if(i==9){
+    id[i].onmouseover = function(e){
+      img[i].style.display="flex";
+    }
+    id[i].onmouseout = function(e){
+      img[i].style.display="none";
+    }
+  }
+}
